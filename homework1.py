c7d1baed59bebc108f872e24d31c00879c13e4e3
AlfaCentawra-coder <vovaluzin55@gmail.com>
2024-07-04 00:12:11 +0500

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,16 @@
+#Строки и ндексация строк 2023
+example = 'Гидроэлектростанция'
+
+print(example[0])
+
+print(example[-1])
+
+print(example[-10::])
+
+print(example[::-1])
+
+print(example[1::2])
+
+
+
+
