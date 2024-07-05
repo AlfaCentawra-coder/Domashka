c0216af60b37194e0aa557f8c17e50e1eb27d60d
AlfaCentawra-coder <vovaluzin55@gmail.com>
2024-07-05 15:02:20 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+count_task = 12
+count_hour = 1.5
+course_name = 'Python'
+time_for_one_task = count_hour / count_task
+print('Курс: ', course_name, ', всего задач: ', count_task, ', затрачено часов: ', count_hour, ', среднее время выполнения задания', time_for_one_task, ' часа')
