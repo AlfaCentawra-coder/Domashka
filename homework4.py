434d8ecd510c04c98b2c84f31cc26faae7ca8d7f
AlfaCentawra-coder <vovaluzin55@gmail.com>
2024-07-08 13:49:33 +0500

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,13 @@
+my_strint = input("Введите свою строку: ")
+
+print (len(my_strint))
+
+print (my_strint.upper())
+
+print (my_strint.lower())
+
+print (my_strint.replace(' ', ''))
+
+print (my_strint[0])
+
+print (my_strint[len(my_strint)-1])
